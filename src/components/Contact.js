8c8d76f1cd6667a8a49a4566ca71eeceb7ee52d8
Jummie <jummiejk21@gmail.com>
2023-09-26 19:42:46 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,21 +14,21 @@ const Contact = () => {
     //   };
   return (
     <>
-      <div className=" bg-blue-primary text-white  pt-12 overflow-y-hidden font-PTSans">
+      <div className=" bg-blue-primary text-white  py-12 overflow-y-hidden font-PTSans">
         <div className="px-28">
           <p className="underline underline-offset-8 decoration-4 decoration-lightb text-[2.1rem] ">
             Contact me
           </p>
         </div>
-        <div className=" font-inter py-8 pl-28 ">
-            <p><span className="font-500">Email: </span>jumokeipoola2019@gmail.com</p>
+        <div className=" font-inter py-4 pl-28 text-[0.8rem] ">
+            <p><span className="">Email: </span>jumokeipoola2019@gmail.com</p>
             <p><span>Phone Number:</span> +234-907-219-809-0</p>
             <div className="pt-4">
             <a
                             href="https://docs.google.com/document/d/1nviURhtmUNJsEEUAk0awVtIyBpEVjlW6ff-XL5OOfms/edit?usp=drivesdk" 
                             target="_blank" 
                             rel="noopener noreferrer"
-                            className="bg-lightb hover:bg-blue-700 text-white mt-8 font-bold py-2 px-6 font-inter"
+                            className="bg-lightb hover:bg-blue-700 text-white mt-8 mb-8 font-bold p-2 px-6 font-inter"
                             >
                             Resume
                         </a>
