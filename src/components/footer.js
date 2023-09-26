d9b4144b1c073fe0059a7125df86b4f2852261ef
Jummie <jummiejk21@gmail.com>
2023-09-26 19:25:01 +0100

    Added social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faFacebook,
+    faTwitter,
+  } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
 
@@ -12,10 +17,21 @@ const Footer = () => {
             <p>Copyright 2023</p>
          </div>
          <div className="flex flex-row">
+            <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="https://www.facebook.com/your-facebook-profile" target="_blank" rel="noopener noreferrer" className="px-12">
+                <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a href="https://twitter.com/your-twitter-profile" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} />
+            </a>
+        </div>
+         {/* <div className="flex flex-row">
             <p>Linkedln</p>
             <p className="px-12">Twiter</p>
             <p>Facebook</p>
-         </div>
+         </div> */}
         </div>
        
       </div>
